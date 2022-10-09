2132fb21082a1f125ad61ea69c707da964d87cdf
Luis Gonzalez <tm.pasadoypresente@gmail.com>
2022-10-09 14:52:19 -0500

    tabla sin localstorage

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-fetch("https://reqres.in/api/users?delay=3")
+fetch("https://reqres.in/api/users?delay=2")
     .then(response => response.json())
     .then(users => printUsers(users.data))
 
-console.log("03 - Después de fetch")
-
 const printUser = (avatar_url, id, email, first_name, last_name) => {
-    return `<div class="col-3 card my-2 mx-2">
-                <img src="${avatar_url}" class="card-img-top"></img>
-                <div class="card-body">
-                    <p>Id: ${id}</p>
-                    <p>email: ${email}</p>
-                    <p>First name: ${first_name}</p>
-                    <p>Last name: ${last_name}</p>
-                </div>
-            <div/>`
+    return `
+
+                <tr class="text-center"><th >${id}<th>${first_name}<th>${last_name}<th>${email}<th><img src="${avatar_url}" class="rounded-circle mx-auto"></img></td>
+           
+         
+            `
 }
 
 const printUsers = (users) => {
-    const container = document.getElementById("users-container")
+    const container = document.getElementById("datos")
     for (let user of users) {
         container.innerHTML += printUser(user.avatar, user.id, user.email, user.first_name, user.last_name)
     }
 }
+
