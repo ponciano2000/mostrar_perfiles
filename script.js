8cf76cc2d0df687132d2b120f89cf6b28494e883
Luis Gonzalez <tm.pasadoypresente@gmail.com>
2022-10-10 12:32:22 -0500

    Versión funcional con detalles

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ fetch("https://reqres.in/api/users?delay=2")
 const printUser = (avatar_url, id, email, first_name, last_name) => {
     return `
 
-                <tr class="text-center"><th >${id}<th>${first_name}<th>${last_name}<th>${email}<th><img src="${avatar_url}" class="rounded-circle mx-auto"></img></td>
+                <trclass="text-center"><td >${id}<td>${first_name}<td>${last_name}<td>${email}<td><img src="${avatar_url}" class="rounded-circle mx-auto" style="width: 90px"></img></td></tr>
            
          
             `
@@ -16,5 +16,20 @@ const printUsers = (users) => {
     for (let user of users) {
         container.innerHTML += printUser(user.avatar, user.id, user.email, user.first_name, user.last_name)
     }
+}
+
+const guardarLocalStorage = data => {
+    const users = {
+        content: [...data],
+        time: Date.now() + 60000
+    }
+    localStorage.setItem('users', JSON.stringify(users))
+}
+
+function readUser() {
+    const user = JSON.parse(localStorage.getItem("userData"));
+    user && user.time > Date.now() ?
+        displayUsers(user.usersData) :
+        fetchRequest();
 }
 
